7420c8a522ec7fd96b5d2ad6a3497997e9ffd5f8
Raif Coonjah <raifcoonjah@proton.me>
2023-10-07 01:00:25 +0400

    fix: wrong file

--- Distro/ArchLinux/arch.sh
+++ Distro/ArchLinux/arch.sh
@@ -48,9 +48,39 @@ aur_apps() {
     yay -S heroic-games-launcher-bin vscodium-bin
 }
 
-# Update mirrorlist using reflector
-echo "Updating mirrorlist..."
-sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+setup_dev() {
+    # Oh my zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # ZSH Plugins
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+    # Docker
+    sudo usermod -aG docker $(whoami)
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    newgrp docker
+
+    # Vagrant
+    # sudo systemctl start --now libvirtd.service
+    # vagrant plugin install vagrant-libvirt
+    # cd ~/
+    # mkdir .dev
+    # cd .dev
+    # mkdir vagrant
+    # cd vagrant
+    # mkdir UbuntuLTS
+    # cd UbuntuLTS
+    # vagrant init generic/debian12
+    # cd ..
+    # mkdir ArchLinux
+    # cd ArchLinux
+    # vagrant init generic/arch
+    
+
+}
+
+
 
 # Install packages from different sources
 echo "[Installing from apps.txt] *****************************************************************************************************************************************************************"
@@ -65,3 +95,7 @@ configure_git
 
 echo "[Installing AUR apps] *****************************************************************************************************************************************************************"
 aur_apps
+
+echo "[Setting up development] *****************************************************************************************************************************************************************"
+install_packages "dev.txt"
+setup_dev
